c407caf4f8a93653733f80a18511d607a1e14f08
IrbIS942 <loglogllog@yandex.ru>
2020-03-16 22:08:53 +0700

    add reading of parameters of figure in main.c

--- main.c
+++ main.c
@@ -27,6 +27,31 @@ void main()
             printf("%s\n", data[count].typename);
         }
 
+    	switch (p->type) {
+    		case 1: {
+				int e = fscanf(fin, " (%d, %d, %d)", &p->params[0], &p->params[1], &p->params[2]);
+				if(e == 3) {
+		        	    printf("parameters \t%s\t x=%d\t y=%d\t r=%d\n", p->typename, p->params[0], p->params[1], p->params[2]);
+	        	} else {
+	        	    printf("vrong parameters \n");
+	        	}
+	            count++;
+	        	break;
+	        }
+	        case 2: {
+	        	int e = fscanf(fin, " (%d, %d, %d, %d, %d, %d, %d, %d)", &p->params[0], &p->params[1], &p->params[2], &p->params[3], &p->params[4], &p->params[5], &p->params[6], &p->params[7]);
+				if(e == 8) {
+		        	printf("parameters \t%s\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\n", p->typename, p->params[0], p->params[1], p->params[2], p->params[3], p->params[4], p->params[5], p->params[6], p->params[7]);
+	        	} else {
+	        	    printf("vrong parameters \n");
+	        	}
+	           	count++;     	
+	        	break;
+	        } 
+		}
+	}
+
+
 	fclose(fin);
 return;
 }
